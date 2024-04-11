a6a03d3ca9bd8931e90638582ac868774842e666
Juan Manuel García del Toro <juanmag.toro@gmail.com>
2024-04-11 10:26:36 +0200

    Add color to footer and other footer changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 import Personajes from "./pages/personajes/Personajes";
 
@@ -12,31 +12,26 @@ import Header from "./core/header/Header";
 import Home from "./pages/home/Home";
 import Episodio from "./pages/episodios/Episodio";
 import Episodios from "./pages/episodios/Episodios";
-
+import Footer from "./core/footer/Footer";
 
 function App() {
-
   return (
     <>
-
       <Router>
         <Header />
         <Routes>
-          <Route path="/" element={ <Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/personajes" element={<Personajes />} />
           <Route path="/character/:id" element={<CharacterDetails />} />
-          <Route path="/locations" element={<LocationsGallery/>} />
-          <Route path="/location/:id" element={<LocationDetails/>} />
-          <Route path="/episodios"  element={  <Episodios />} />
-          <Route path="/episodio/:id"  element={  <Episodio />} />
-
+          <Route path="/locations" element={<LocationsGallery />} />
+          <Route path="/location/:id" element={<LocationDetails />} />
+          <Route path="/episodios" element={<Episodios />} />
+          <Route path="/episodio/:id" element={<Episodio />} />
         </Routes>
-        
-      
+        <Footer />
       </Router>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
